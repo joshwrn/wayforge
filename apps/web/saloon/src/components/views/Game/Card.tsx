8a165cd134872f4665e731717def18fb95b77559
Jeremy Banka <hello@jeremybanka.com>
2023-08-07 13:14:56 -0700

    🚧

--- apps/web/saloon/src/components/views/Game/Card.tsx
+++ apps/web/saloon/src/components/views/Game/Card.tsx
@@ -1,12 +1,10 @@
-import { css } from "@emotion/react"
 import { useO } from "atom.io/react"
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import type { FC } from "react"
 
 import { valuesOfCardsState } from "~/apps/node/lodge/src/store/game"
-import * as PLAYING_CARDS from "~/apps/node/lodge/src/store/game/playing-card-data"
 
-import { article } from "../../containers/<article>"
+import scss from "./Card.module.scss"
 import { PlayingCards } from "../../PlayingCards"
 
 export const CardFace: FC<{ id: string }> = ({ id }) => {
@@ -14,18 +12,18 @@ export const CardFace: FC<{ id: string }> = ({ id }) => {
 	const PlayingCard = PlayingCards[value as keyof typeof PlayingCards]
 	return (
 		<AnimatePresence>
-			<article.roundedWhite layoutId={id}>
+			<motion.article className={scss.class} layoutId={id}>
 				{PlayingCard ? <PlayingCard /> : null}
-			</article.roundedWhite>
+			</motion.article>
 		</AnimatePresence>
 	)
 }
 export const CardBack: FC<{ id: string }> = ({ id }) => {
 	return (
 		<AnimatePresence>
-			<article.roundedWhite layoutId={id}>
+			<motion.article className={scss.class} layoutId={id}>
 				{/* <PlayingCards.Back /> */}
-			</article.roundedWhite>
+			</motion.article>
 		</AnimatePresence>
 	)
 }
